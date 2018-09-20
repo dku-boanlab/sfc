594e82ada85ffc51523d27f3afd38c47f2494a9b
Jaehyun Nam <namjh@kaist.ac.kr>
2018-09-20 12:45:21 -0700

    minor updates

--- setup/kvm-install.sh
+++ setup/kvm-install.sh
@@ -4,3 +4,6 @@
 sudo apt-get install -y qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils uuid \
                         linux-tools-common linux-tools-`uname -r` \
                         linux-tools-virtual virt-top virt-manager
+
+# install psutil
+sudo dpkg -i python-psutil_1.2.1-1ubuntu2_amd64.deb
