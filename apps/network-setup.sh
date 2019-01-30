e64d7df1c7a0611ea269ffd5fff6bbe7bfcef2dc
Jaehyun Nam <namjh@kaist.ac.kr>
2019-01-30 19:59:12 +0900

    minor updates

--- apps/network-setup.sh
+++ apps/network-setup.sh
@@ -8,9 +8,7 @@ elif [ -z $2 ]; then
 	exit
 fi
 
-# change hostname
-sudo sed -i 's/ubuntu/'$1'/g' /etc/hosts
-sudo sed -i 's/ubuntu/'$1'/g' /etc/hostname
-
-# change IP address
-sudo sed -i 's/192.168.122.10/'$2'/g' /etc/network/interfaces
+# change hostname and IP address
+sudo sed -i 's/ubuntu/$1/g' /etc/hosts
+sudo sed -i 's/ubuntu/$1/g' /etc/hostname
+sudo sed -i 's/192.168.122.10/$2/g' /etc/network/interfaces
