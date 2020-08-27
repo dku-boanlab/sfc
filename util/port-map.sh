527136197a6c7db5270d76cc406da0fc5e783e37
Jaehyun Nam <67940721+sdxdata-namjh@users.noreply.github.com>
2020-08-27 15:51:23 +0900

    Update port-map.sh

--- util/port-map.sh
+++ util/port-map.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# print the mapping of ports and interfaces
+# print port-to-interface mappings
 sudo ovs-ofctl show ovsbr0 | grep addr | grep -v LOCAL | awk -F"(" '{print "Port:" $1 " Interface: " $2}' | awk -F")" '{print $1}'
